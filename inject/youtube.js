c70634a16887298656c05bb37004058c39ab75d3
nonk123 <me@nonk.dev>
2026-01-05 15:12:59 +0300

    janky ahh button styling (part 1)

--- inject/youtube.js
+++ inject/youtube.js
@@ -1,27 +1,44 @@
 (function () {
+    const OUR_BUTTON_ID = "lyrix";
+    const CONTAINER_ID = "top-level-buttons-computed";
+    const BUTTON_CLASS = "yt-spec-button-shape-next yt-spec-button-shape-next--tonal yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-m";
+
     function appendLyrixButton() {
+        const outer = document.createElement("yt-button-view-model");
+        outer.className = "ytd-menu-renderer";
+
+        const shape = document.createElement("yt-button-shape");
+        outer.appendChild(shape);
+
         const button = document.createElement("button");
-        button.textContent = "Lrx", button.id = "lyrix";
+        button.textContent = "Lrx", button.id = OUR_BUTTON_ID;
+        button.className = BUTTON_CLASS;
         button.addEventListener("click", () => {
             chrome.runtime.sendMessage({
                 action: "openLyrics",
                 title: document.getElementById("title").textContent,
             });
         });
-        document.getElementById("actions").appendChild(button);
+        shape.appendChild(button);
+
+        const container = document.getElementById(CONTAINER_ID);
+        container.appendChild(outer);
+
         console.info("Lyrix button attached");
     }
 
     const observer = new MutationObserver((mutationList, observer) => {
         for (const mutation of mutationList) {
+            if (document.getElementById(OUR_BUTTON_ID))
+                return;
             if (mutation.type !== "childList")
                 continue;
             for (const node of mutation.addedNodes)
-                if (node.id === "actions")
+                if (node.id === CONTAINER_ID) {
                     appendLyrixButton();
+                    break;
+                }
         }
     });
-
-    const config = { childList: true, subtree: true };
-    observer.observe(document.body, config);
+    observer.observe(document.body, { childList: true, subtree: true });
 })();
