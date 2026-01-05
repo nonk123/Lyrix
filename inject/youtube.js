23e9612a4ff424d71ba174e25751b09de89edcce
nonk123 <me@nonk.dev>
2026-01-05 17:05:18 +0300

    insane hack: make the look up lyrics button a context-menu item

--- inject/youtube.js
+++ inject/youtube.js
@@ -1,42 +1,42 @@
 (function () {
     const OUR_BUTTON_ID = "lyrix";
-    const CONTAINER_ID = "player-container";
-    const BUTTON_STYLE = (function () {
-        const padding = "8px";
-        return `
-position: absolute;
-right: ${padding};
-top: ${padding};
-`;
-    })();
+
+    const menuItem = document.createElement("div");
+    menuItem.className = "ytp-menuitem", menuItem.role = "menuitem", menuItem.tabIndex = "0";
+    menuItem.ariaHasPopup = "false";
+
+    const icon = document.createElement("div");
+    icon.className = "ytp-menuitem-icon";
+
+    const label = document.createElement("div");
+    label.className = "ytp-menuitem-label";
+    label.textContent = "Look up lyrics";
+
+    const content = document.createElement("div");
+    content.className = "ytp-menuitem-content";
+
+    menuItem.id = OUR_BUTTON_ID;
+    menuItem.addEventListener("click", () => {
+        chrome.runtime.sendMessage({
+            action: "openLyrics",
+            title: document.getElementById("title").textContent,
+        });
+    });
+    menuItem.append(icon, label, content);
 
     function appendLyrixButton() {
-        const button = document.createElement("button");
-        button.id = OUR_BUTTON_ID, button.style = BUTTON_STYLE;
-        button.textContent = "Lyrix";
-        button.addEventListener("click", () => {
-            chrome.runtime.sendMessage({
-                action: "openLyrics",
-                title: document.getElementById("title").textContent,
-            });
-        });
+        const popups = document.querySelectorAll(".ytp-popup.ytp-contextmenu");
+        for (const popup of popups)
+            popup.style.height = ""; // a scrollbar appears otherwise
 
-        document.getElementById(CONTAINER_ID).appendChild(button);
-        console.info("Lyrix button attached");
+        const candidates = document.querySelectorAll(".ytp-popup.ytp-contextmenu .ytp-panel-menu");
+        for (const container of candidates)
+            container.appendChild(menuItem);
     }
 
-    const observer = new MutationObserver((mutationList, observer) => {
-        for (const mutation of mutationList) {
-            if (document.getElementById(OUR_BUTTON_ID) !== null)
-                return;
-            if (mutation.type !== "childList")
-                continue;
-            for (const node of mutation.addedNodes)
-                if (node.id === CONTAINER_ID) {
-                    appendLyrixButton();
-                    break;
-                }
-        }
+    const observer = new MutationObserver((mutations, observer) => {
+        if (!document.getElementById(OUR_BUTTON_ID))
+            appendLyrixButton();
     });
     observer.observe(document.body, { childList: true, subtree: true });
 })();
